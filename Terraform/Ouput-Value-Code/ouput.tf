8aacb8fb07a12cfa66830b9937dba1e3d68d8e1e
rohit260 <rohitd@trinesis.com>
2024-09-26 17:00:43 +0530

    Output the specific value on console with code modify

--- Terraform/Ouput-Value-Code/ouput.tf
+++ Terraform/Ouput-Value-Code/ouput.tf
@@ -6,6 +6,13 @@ resource "aws_eip" "my_eip" {
   domain = "vpc"
 }
 
+//Code only print public ip id on screen for Elastic Ip resouce
 output "Firewall_public_id" {
     value = "${aws_eip.my_eip.public_ip}"
+}
+
+//code print all the attribute populated with the Elastic Ip resource
+
+output "FireWall_Attributes" {
+    value = "${aws_eip.my_eip}"
 }
